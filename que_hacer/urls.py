bbac3bf0cc968f2443d4eeaab02350bf4bf5b5dd
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-04-28 13:11:11 -0500

    paso 04 - registro de tareas

--- que_hacer/urls.py
+++ que_hacer/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'que_hacer'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registro/<int:grupo_id>/', views.registro, name='registro'),
 ]
--- que_hacer/views.py
+++ que_hacer/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Grupo, QueHacer
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     grupo_lista = Grupo.objects.all()
     contexto = {'grupo_lista': grupo_lista}
     return render(request, 'que_hacer/index.html', contexto)
+
+
+def registro(request, grupo_id):
+    if request.POST['tarea'] == '':
+        return HttpResponseRedirect(reverse('que_hacer:index'))
+    grupo = Grupo.objects.get(pk=grupo_id)
+    grupo.quehacer_set.create(tarea=request.POST['tarea'])
+    return HttpResponseRedirect(reverse('que_hacer:index'))
