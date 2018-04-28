8ff019f8e8585b12f72fe735625532d1a11d2695
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-04-28 01:19:46 -0500

    cambio configuracion

--- fsback/settings.py
+++ fsback/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'que_hacer',
+    'que_hacer.apps.QueHacerConfig',
 ]
 
 MIDDLEWARE = [
