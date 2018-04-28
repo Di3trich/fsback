46222347b64a6c47c507ffeb94568c4b01c64782
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-04-28 11:12:06 -0500

    modelos

--- que_hacer/models.py
+++ que_hacer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha = models.DateField('fecha de publicacion')
+
+    def __str__(self):
+        return self.nombre
+
+
+class QueHacer(models.Model):
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    tarea = models.CharField(max_length=300)
+    realizado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s [%s]" % (self.tarea, 'completado' if self.realizado else 'pendiente')
