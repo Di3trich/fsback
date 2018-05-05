25984faf75e0748ff244ce59a60edda7cf02ce76
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-05-05 15:26:07 -0500

    creador

--- que_hacer/serializers.py
+++ que_hacer/serializers.py
@@ -9,11 +9,18 @@ class QueHacerListaSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class GrupoSerializer(serializers.HyperlinkedModelSerializer):
-    quehacer_set = QueHacerListaSerializer(many=True, read_only=True)
+    quehacer_set = QueHacerListaSerializer(many=True)
 
     class Meta:
         model = Grupo
         fields = ('url', 'nombre', 'fecha', 'quehacer_set')
+
+    def create(self, validated_data):
+        quehacer_data = validated_data.pop('quehacer_set')
+        grupo = Grupo.objects.create(**validated_data)
+        for quehacer in quehacer_data:
+            QueHacer.objects.create(grupo=grupo, **quehacer)
+        return grupo
 
 
 class QueHacerSerializer(serializers.HyperlinkedModelSerializer):
