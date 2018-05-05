bdb86121be3b591b23e3884cf5af6c404c77bf62
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-05-05 15:11:56 -0500

    serializador relacional

--- que_hacer/serializers.py
+++ que_hacer/serializers.py
@@ -2,10 +2,18 @@ from .models import Grupo, QueHacer
 from rest_framework import serializers
 
 
+class QueHacerListaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = QueHacer
+        fields = ('url', 'tarea', 'realizado')
+
+
 class GrupoSerializer(serializers.HyperlinkedModelSerializer):
+    quehacer_set = QueHacerListaSerializer(many=True, read_only=True)
+
     class Meta:
         model = Grupo
-        fields = ('url', 'nombre', 'fecha')
+        fields = ('url', 'nombre', 'fecha', 'quehacer_set')
 
 
 class QueHacerSerializer(serializers.HyperlinkedModelSerializer):
