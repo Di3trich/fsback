b8ea1e1060ab7de66104584de3e00a30bc3933a3
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-05-11 13:31:11 -0500

    permitir todo

--- fsback/settings.py
+++ fsback/settings.py
@@ -54,6 +54,12 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
+
 ROOT_URLCONF = 'fsback.urls'
 
 TEMPLATES = [
